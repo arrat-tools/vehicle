b4986eac1c3ec5142ba5d1750ebd2864be85e8cd
arrat-tools <I70TACadmin@HNTB.com>
2025-11-07 14:32:35 -0500

    Update data_sync.sh
    Ensure sync_dir matches default from gui.yaml
    
    Has s3Location be passed into script to match documentation

--- ra-daq-gui/scripts/data_sync.sh
+++ ra-daq-gui/scripts/data_sync.sh
@@ -1,10 +1,10 @@
 #! /bin/sh
 
 ## this is the local folder where the current units reside
-sync_dir=/tmp/collect
+sync_dir=/road_audit_data/sync
 
 ## this is the s3 bucket and folder where the collect will be uploaded to
-s3location=s3://tac-image-staging/
+s3location=$1
 timestamp=$(date +"%Y-%m-%d-%H-%M")
 
 ## this will sync all files and folders in the local folder to the s3 bucket
